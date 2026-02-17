878162361a273836921d6a3a6a96d33bac0c00a5
meejah <meejah@meejah.ca>
2026-02-17 15:05:12 -0700

    kind-of works, but can't see end of stream

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, RequestMode, Response};
+use web_sys::{Request, RequestInit, RequestMode, Response, ReadableStreamDefaultReader};
 use magic_cap;
 use std::io::Cursor;
+use js_sys::{JsString, Object, Uint8Array};
 
 // Called when the Wasm module is instantiated
 #[wasm_bindgen(start)]
@@ -40,15 +41,36 @@ async fn main() -> Result<(), JsValue> {
     console::log_1(&"about to do future stuff".into());
 
     // Convert this other `Promise` into a rust `Future`.
-    let jsvalue = JsFuture::from(resp.text()?).await?;
-    let string = jsvalue.as_string().ok_or("it's not a string i guess")?;
-    let text = string.as_str();
-    console::log_2(&"got text".into(), &JsValue::from(text.len() as f32));
+    let stream = resp.body().ok_or("no kittens")?;
+    let reader: ReadableStreamDefaultReader = stream.get_reader().dyn_into().expect("reader failed");
+
+
+    let mut data: Vec<u8> = vec![];
+
+    let mut result_value = JsFuture::from(reader.read()).await;
+    let mut done = false;
+    while !done {
+        if let Ok(stuff) = result_value {
+            console::log_1(&"trying to load a chunk".into());
+            let result: Object = stuff.dyn_into().unwrap();
+            console::log_1(&format!("result is {:?}", result).into());
+            let chunk_value = js_sys::Reflect::get(&result, &JsValue::from_str("value")).unwrap();
+            // next line fails when we're done
+            let chunk_array: Uint8Array = chunk_value.dyn_into().unwrap();
+            let mut chunk = chunk_array.to_vec();
+            console::log_1(&format!("got some {}", chunk.len()).into());
+            data.append(&mut chunk);
+            result_value = JsFuture::from(reader.read()).await;
+        } else {
+            done = true;
+        }
+    }
 
     console::log_1(&"loading mcap".into());
+    console::log_1(&format!("chunk size: {}", data.len()).into());
 
     // this just "unreachables" into the js console -- can we error-handle better?
-    let mc = match magic_cap::Immutable::read(Cursor::new(text)) {
+    let mc = match magic_cap::Immutable::read(Cursor::new(data)) {
         Ok(x) => {"loaded successfully"}
         Err(e) => {"error loading mcap"}
     };
