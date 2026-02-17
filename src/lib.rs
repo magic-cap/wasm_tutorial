f16c5388fe16a52edf4f9ca55a6aba2c79d52345
meejah <meejah@meejah.ca>
2026-02-17 14:24:18 -0700

    something worked, kind of

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,34 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Response};
+use magic_cap;
+use std::io::Cursor;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+// Called when the Wasm module is instantiated
+#[wasm_bindgen(start)]
+async fn main() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
-}
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+    body.append_child(&val)?;
 
+    use web_sys::console;
 
-#[wasm_bindgen]
-pub async fn run(repo: String) -> Result<JsValue, JsValue> {
     let opts = RequestInit::new();
     opts.set_method("GET");
     opts.set_mode(RequestMode::Cors);
 
-    let url = format!("https://api.github.com/repos/{repo}/branches/master");
+    //let url = format!("https://github.com/magic-cap/magic-cap/raw/refs/heads/main/kitten.mcap");
+    let url = format!("kitten.mcap");
 
     let request = Request::new_with_str_and_init(&url, &opts)?;
-
-    request
-        .headers()
-        .set("Accept", "application/vnd.github.v3+json")?;
+    request.headers().set("Accept", "application/binary")?;
 
     let window = web_sys::window().unwrap();
     let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
@@ -34,9 +37,31 @@ pub async fn run(repo: String) -> Result<JsValue, JsValue> {
     assert!(resp_value.is_instance_of::<Response>());
     let resp: Response = resp_value.dyn_into().unwrap();
 
-    // Convert this other `Promise` into a rust `Future`.
-    let json = JsFuture::from(resp.json()?).await?;
+    console::log_1(&"about to do future stuff".into());
 
-    // Send the JSON response back to JS.
-    Ok(json)
+    // Convert this other `Promise` into a rust `Future`.
+    let jsvalue = JsFuture::from(resp.text()?).await?;
+    let string = jsvalue.as_string().ok_or("it's not a string i guess")?;
+    let text = string.as_str();
+    console::log_2(&"got text".into(), &JsValue::from(text.len() as f32));
+
+    console::log_1(&"loading mcap".into());
+
+    // this just "unreachables" into the js console -- can we error-handle better?
+    let mc = match magic_cap::Immutable::read(Cursor::new(text)) {
+        Ok(x) => {"loaded successfully"}
+        Err(e) => {"error loading mcap"}
+    };
+
+    let foo = format!("mcap: {:?}", mc);
+    let val = document.create_element("p")?;
+    val.set_inner_html(foo.as_str());
+    body.append_child(&val)?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn add(a: u32, b: u32) -> u32 {
+    a + b
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
-// use crate::lib::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
-fn main() {
-    println!("Hello, world!");
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {name}!"));
+}
+
+
+#[wasm_bindgen]
+pub async fn run(repo: String) -> Result<JsValue, JsValue> {
+    let opts = RequestInit::new();
+    opts.set_method("GET");
+    opts.set_mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/repos/{repo}/branches/master");
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Send the JSON response back to JS.
+    Ok(json)
 }
